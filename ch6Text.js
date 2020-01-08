24e5ec3e335e885de69838fefb564749149bed79
Ben Rachbach <brachbach@gmail.com>
2020-01-07 20:43:51 -0800

    Start reading ch 6

---
+++ ch6Text.js
@@ -0,0 +1,5 @@
+- what are the rules for updating a uniform prior? This just gets swept under the rug here i.e. gets done by probMods, right?
+- for the candidates -- why does the prior model dip at the extremes (0 and 1)?
+- in the helping example -- you have to realize that "research assistant" is relevant,
+and that e.g. "time of day" is not relevant, to realize that you should slice the data like this, right?
+- 
