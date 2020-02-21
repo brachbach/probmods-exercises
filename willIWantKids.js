5a7d693c8d806a46599e16e61f1d88dd48e2a8b7
Ben Rachbach <brachbach@gmail.com>
2020-02-20 19:47:30 -0800

    Start playing with state machine

--- willIWantKids.js
+++ willIWantKids.js
@@ -4,6 +4,103 @@
 // So assume that the current economy will continue over the entire time period
 // (no GCRs, no extremely transformative AI).
 
+// state-machine based approach
+
+var addNextNumber = function(numbersSoFar, length) {
+  if (numbersSoFar.length === length) {
+    return numbersSoFar;
+  }
+  return addNextNumber(numbersSoFar.concat([numbersSoFar.length]), length);
+};
+
+var getNWholeNumbers = function(n) {
+  return addNextNumber([], n);
+};
+
+var states = {
+  openToIt: {
+    description:
+      "open to having kids if a partner who I was ~married to wanted to have them",
+    transitions: [
+      {
+        toState: states.lookForPartner,
+        // hmmm, is this (or at least how I'm plannign to use it) doing manually what WebPPL is supposed to do automatically?
+        ps: [
+          {
+            p: 0.1,
+            startYear: 0,
+            endYear: 1
+          },
+          {
+            p: 0.3,
+            startYear: 0,
+            endYear: 5
+          }
+        ]
+      },
+      {
+        toState: states.doNotWant,
+        ps: [
+          {
+            p: 0.05,
+            startYear: 0,
+            endYear: 1
+          },
+          {
+            p: 0.2,
+            startYear: 0,
+            endYear: 5
+          }
+        ]
+      }
+    ]
+  },
+  lookForPartner: {
+    description:
+      "want to have kids, actively looking for a partner to have kids with",
+    transitions: [
+      {
+        toState: states.openToIt,
+        ps: [
+          {
+            p: 0.3,
+            startYear: 0,
+            endYear: 20
+          },
+          {
+            p: 0.9,
+            startYear: 20,
+            endYear: 50
+          }
+        ]
+      },
+      {
+        // maybe sort of weird to incorporate everything in one state machine
+        // rather than having separate state machines for desire to have kids, relationship progress, etc.
+        toState: states.inKidsRelationship,
+        ps: map(getNWholeNumbers(50), function(startYear) {
+          return {
+            p: 0.7,
+            startYear: startYear,
+            endYear: startYear + 1
+          };
+        })
+      },
+      {
+        toState: states.openToIt,
+        // actually should prob increase as time goes on
+        ps: map(getNWholeNumbers(50), function(startYear) {
+          return {
+            p: 0.7,
+            startYear: startYear,
+            endYear: startYear + 1
+          };
+        })
+      }
+    ]
+  }
+};
+
 var censusFactor = function(weight) {
   return {
     name: "Census analysis",
